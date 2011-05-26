b815100984d82c912881d71f377b0f0402f8df3c
unknown <kthompson@.devnet.dom>
2011-05-26 14:02:45 -0400

    Added code to build simple display driver and automate building a floppy image from kernel and GRUB stages

--- build.bat
+++ build.bat
@@ -1,13 +1,21 @@
 echo Now assembling, compiling, and linking your kernel:
+
 nasm -f aout -o start.o start.asm
 rem Remember this spot here: We will add 'gcc' commands here to compile C sources
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
-
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
 
 
 rem This links all your files. Remember that as you add *.o files, you need to
 rem add them after start.o. If you don't add them at all, they won't be in your kernel!
-ld -T link.ld -o kernel.bin main.o start.o
-echo Done!
+ld -T link.ld -o kernel.bin start.o main.o  scrn.o
+echo Build Done!
+
+echo Proceeding to generate Bochs Image for booting
+
+del floppy.img
+copy /b stage1+stage2+pad+kernel.bin floppy.img
+
+
 pause
