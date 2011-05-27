31514f396090bea37d8a102d1d288e0377df188c
unknown <kthompson@.devnet.dom>
2011-05-27 18:14:41 -0400

    Added new lines for isr code

--- build.bat
+++ build.bat
@@ -5,11 +5,13 @@ rem Remember this spot here: We will add 'gcc' commands here to compile C source
 
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o isr.o isr.c
 
 
 rem This links all your files. Remember that as you add *.o files, you need to
 rem add them after start.o. If you don't add them at all, they won't be in your kernel!
-ld -T link.ld -o kernel.bin start.o main.o  scrn.o
+ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o isr.o
 echo Build Done!
 
 echo Proceeding to generate Bochs Image for booting
