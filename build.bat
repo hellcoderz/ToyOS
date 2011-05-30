9f1325f1b74129027675fd8e37d93f888c22a7fb
Kurtis Thompson <kurtis.thompson@gmail.com>
2011-05-30 19:59:54 -0400

    Updated batch file for building

--- build.bat
+++ build.bat
@@ -7,11 +7,12 @@ gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
 gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o isr.o isr.c
-
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o timer.o timer.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o kb.o kb.c
 
 rem This links all your files. Remember that as you add *.o files, you need to
 rem add them after start.o. If you don't add them at all, they won't be in your kernel!
-ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o isr.o
+ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o isr.o timer.o kb.o
 echo Build Done!
 
 echo Proceeding to generate Bochs Image for booting
